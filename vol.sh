471e460aa82139e41b307506a7616ef2460edb79
RalphDesmangles <35283608+RalphDesmangles@users.noreply.github.com>
2021-04-15 01:43:14 -0400

    Create vol.sh

---
+++ vol.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir Analysis
+mkdir Analysis/Tools/
+
+cd Analysis
+wget http://files.sempersecurus.org/dumps/cridex_memdump.zip
+unzip cridex_memdump.zip
+rm cridex_memdump.zip
+
+cd Tools
+wget http://downloads.volatilityfoundation.org/releases/2.6/volatility_2.6_lin64_standalone.zip
+unzip volatility_2.6_lin64_standalone.zip
+rm volatility_2.6_lin64_standalone.zip
+
+mv volatility_2.6_lin64_standalone.zip volatility
+
+cd volatility
+mv volatility_2.6_lin64_standalone volatility_2.6
